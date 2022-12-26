f6d52d787853a1314b9315572d10932e4baa6cfa
abhay-rana <abhayrana286@gmail.com>
2022-12-26 20:00:09 +0530

    divider component

--- src/components/common/divider.js
+++ src/components/common/divider.js
@@ -1,9 +1,70 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
-const Divider = () => {
+const Divider = ({ className, vertical, children, gap, textAlign, marginLeft, marginRight, color, textColor }) => {
+	let extra_class = {
+		left: "",
+		right: "",
+		align: "",
+		gap: "",
+		marginLRTB: {},
+		color: "",
+		textColor: "",
+		direction: "flex-row",
+		border: "border-t-1",
+	};
+	if (vertical) {
+		extra_class.direction = "flex-col ";
+		extra_class.border = "border-l-1 ";
+	}
+	if (textAlign === "left") extra_class.left += `flex-none `;
+	if (textAlign === "right") extra_class.right += `flex-none `;
+	// if (textAlign === "top") extra_class.align = `flex-none `;
+	// if (textAlign === "bottom") extra_class.align = `flex-none `;
+	if (color) extra_class.color += color;
+	if (textColor) extra_class.textColor += textColor;
+
+	if (textAlign === "left" && gap && !marginLeft) {
+		extra_class.marginLRTB = { marginRight: `${gap}px` };
+	} else if (textAlign === "right" && gap && !marginRight) {
+		extra_class.marginLRTB = { marginLeft: `${gap}px` };
+	} else if (textAlign === "top" && gap && !marginRight) {
+		extra_class.marginLRTB = { marginTop: `${gap}px` };
+	} else if (textAlign === "bottom" && gap && !marginRight) {
+		extra_class.marginLRTB = { marginBottom: `${gap}px` };
+	}
+
+	if (textAlign && !marginLeft && !marginRight) {
+		extra_class.gap += `0`;
+	} else {
+		extra_class.gap += gap;
+	}
+
 	return (
 		<>
-			<div>Divider</div>
+			<div
+				className={`flex items-center text-center ${extra_class.direction}`}
+				style={{ gap: `${extra_class.gap}px` }}
+			>
+				<div
+					className={twMerge(`flex-1 border-black`, extra_class.left, extra_class.color, extra_class.border)}
+					style={{ width: `${marginLeft}px` }}
+				></div>
+
+				{children ? (
+					<div
+						style={extra_class.marginLRTB}
+						className={extra_class.textColor}
+					>
+						{children}
+					</div>
+				) : null}
+
+				<div
+					className={twMerge(`flex-1 border-black`, extra_class.right, extra_class.color, extra_class.border)}
+					style={{ width: `${marginRight}px` }}
+				></div>
+			</div>
 		</>
 	);
 };
--- src/screens/divider-test.js
+++ src/screens/divider-test.js
@@ -5,8 +5,112 @@ import Divider from "components/common/divider";
 const DividerTest = () => {
 	return (
 		<>
-			<div>DividerTest</div>
-			<Divider />
+			<div className="m-4 text-center">DividerTest</div>
+
+			<div className="relative flex h-60 flex-col border border-black p-1">
+				<div className="mb-4 text-center">Horizontal Lines with Text</div>
+
+				{/*simple horizontal rule*/}
+				<Divider color={"border-red-400"} />
+				{/* text in the center */}
+				<Divider color={"border-slate-400"}>Center</Divider>
+				{/* text in center with gap */}
+				<Divider
+					gap={10}
+					textColor={"text-primary"}
+					color="border-blue-400"
+				>
+					Center Gap
+				</Divider>
+				{/* textAlign to the left without gap */}
+				<Divider textAlign="left">Left</Divider>
+				{/* textAlign to the left with gap */}
+				<Divider
+					textAlign="left"
+					gap={10}
+				>
+					Left gap
+				</Divider>
+				{/* textAlign to the right without gap */}
+				<Divider textAlign="right">Right</Divider>
+				{/* textAlign to the right with gap */}
+				<Divider
+					textAlign="right"
+					gap={10}
+				>
+					Right Gap
+				</Divider>
+				{/* text align to the left with marginLeft */}
+				<Divider
+					gap={5}
+					textAlign="left"
+					marginLeft={20}
+					className={"border-dashed border-primary"}
+				>
+					Left Margin
+				</Divider>
+				{/* text align to the right with marginRight */}
+				<Divider
+					gap={5}
+					textAlign="right"
+					marginRight={30}
+					className={"border-dashed border-primary"}
+				>
+					Right Margin
+				</Divider>
+			</div>
+			<div className="relative my-10 flex h-40 flex-col border border-black p-1">
+				<div className="text-center">Vertical Line with text</div>
+				<div className="flex h-full flex-row border border-black">
+					<div>Vertical</div>
+					<Divider
+						color="border-pink-300"
+						vertical
+					/>
+					<div>Testing</div>
+					<div className="ml-4">Vertical</div>
+					<Divider
+						color="border-pink-300"
+						textColor={"text-warning"}
+						vertical
+					>
+						Hello
+					</Divider>
+					<div>Testing</div>
+					<div className="ml-4">Vertical</div>
+					<Divider
+						color="border-pink-300"
+						textColor={"text-warning"}
+						vertical
+						gap={5}
+					>
+						Hello
+					</Divider>
+					<div>Testing</div>
+					<div className="ml-4">Vertical</div>
+					<Divider
+						color="border-pink-300"
+						textColor={"text-warning"}
+						vertical
+						textAlign="top"
+					>
+						Hello
+					</Divider>
+					<div>Testing</div>
+				</div>
+				{/* <div
+					className="flex flex-col items-center border border-black text-center"
+					style={{ gap: "0px" }}
+				>
+					<div
+						className="border-l-1 border-black"
+						style={{ height: "10px" }}
+					></div>
+					<div className="">Hello</div>
+					<div className="flex-1 border-l-1 border-black"></div>
+				</div>
+				<div>test</div> */}
+			</div>
 		</>
 	);
 };
