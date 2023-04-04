3ba494f04c5f07f8f24ea10b0b1b2a0a5059286b
abhay-rana <abhayrana286@gmail.com>
2023-04-05 00:24:10 +0530

    skeleton component

--- src/screens/skeleton-test.js
+++ src/screens/skeleton-test.js
@@ -5,16 +5,16 @@ import Skeleton from 'components/skeleton/skeleton';
 const SkeletonTest = () => {
     return (
         <>
-            <div className="flex flex-col  min-h-full">
-                <div className="text-center text-lg text-red-400 border-2 border-black">
+            <div className="flex min-h-full  flex-col">
+                <div className="border-2 border-black text-center text-lg text-red-400">
                     Skeleton Effect
                 </div>
-                <div className="flex-1 flex flex-row">
+                <div className="flex flex-1 flex-row">
                     <div className="w-[250px] border border-black p-4">
                         Variants
                     </div>
-                    <div className="flex flex-col flex-1 gap-10 border border-black p-4">
-                        <div className="w-[500px] border border-black shadow-lg p-1">
+                    <div className="flex flex-1 flex-col gap-10 border border-black p-4">
+                        <div className="w-[500px] border border-black p-1 shadow-lg">
                             <Skeleton
                                 type="square"
                                 height={55}
@@ -23,20 +23,20 @@ const SkeletonTest = () => {
                             />
                             sqaure skeleon without animation
                         </div>
-                        <div className="w-[500px] border border-black shadow-lg p-1">
+                        <div className="w-[500px] border border-black p-1 shadow-lg">
                             <Skeleton type="square" height={55} width={55} />
                             default animation is on
                         </div>
-                        <div className="w-[500px] border border-black shadow-lg p-1">
+                        <div className="w-[500px] border border-black p-1 shadow-lg">
                             <Skeleton type="circle" height={55} width={55} />
                             circle skeleton (configrue height and width by
                             props)
                         </div>
-                        <div className="w-[500px] border border-black shadow-lg p-1">
+                        <div className="w-[500px] border border-black p-1 shadow-lg">
                             <Skeleton type="line" />
                             line defualt (width is 100%)
                         </div>
-                        <div className="w-[500px] border border-black shadow-lg p-1">
+                        <div className="w-[500px] border border-black p-1 shadow-lg">
                             <Skeleton
                                 type="line"
                                 style={{ bg_color: 'bg-yellow-100' }}
